938d3fe4f869e60b22b398b491ab7b3795cb3840
Suman Rayamajhi <meh_sams@yahoo.com>
2022-03-22 18:53:52 +0100

    error correction

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -73,18 +73,18 @@ const Board = () => {
       return grid[0][0];
     }
     if (
-      grid[1][0] === grid[1][1] &&
-      grid[1][1] === grid[1][2] &&
-      grid[1][0] !== ""
+      grid[0][1] === grid[1][1] &&
+      grid[1][1] === grid[2][1] &&
+      grid[0][1] !== ""
     ) {
-      return grid[1][0];
+      return grid[0][1];
     }
     if (
-      grid[2][0] === grid[2][1] &&
-      grid[2][1] === grid[2][2] &&
-      grid[2][0] !== ""
+      grid[0][2] === grid[1][2] &&
+      grid[1][2] === grid[2][2] &&
+      grid[0][2] !== ""
     ) {
-      return grid[2][0];
+      return grid[0][2];
     }
     if (
       grid[0][0] === grid[1][1] &&
@@ -94,8 +94,8 @@ const Board = () => {
       return grid[0][0];
     }
     if (
-      grid[0][2] === grid[2][2] &&
-      grid[2][2] === grid[2][0] &&
+      grid[0][2] === grid[1][1] &&
+      grid[1][1] === grid[2][0] &&
       grid[0][2] !== ""
     ) {
       return grid[0][2];
